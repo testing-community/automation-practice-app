e2801ed0153eb822da57b880c493a2ba8c162a38
Alejandro Perdomo <17033942+aperdomob@users.noreply.github.com>
2022-05-21 14:28:50 -0500

    feat(skills): add pagination

--- src/main/java/testing/community/automation/practice/app/controllers/SkillsControllers.java
+++ src/main/java/testing/community/automation/practice/app/controllers/SkillsControllers.java
@@ -1,11 +1,13 @@
 package testing.community.automation.practice.app.controllers;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import testing.community.automation.practice.app.controllers.dto.Skill;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.util.*;
 
 @RestController
 @RequestMapping("skills")
@@ -14,15 +16,31 @@ public class SkillsControllers {
 
     public SkillsControllers() {
         skills = new HashMap<>() {{
-            put(UUID.randomUUID().toString(), "Java");
-            put(UUID.randomUUID().toString(), "Selenium");
-            put(UUID.randomUUID().toString(), "Automation API Testing");
+            put("671645b5-306f-4328-9b32-0c19873373e5", "Java");
+            put("9269d596-8e54-43c4-93c8-d8ee438abbe1", "Selenium");
+            put("5cb5c0a9-f2be-4daa-9cf0-3d69caba5590", "Automation API Testing");
         }};
     }
 
     @GetMapping()
-    public List<Skill> getSkills() {
-        return skills.entrySet().stream().map(entry -> new Skill(entry.getKey(), entry.getValue())).toList();
+    public List<Skill> getSkillsPaginated(
+            @RequestParam(name ="page", defaultValue = "0") @Min(0) final Optional<Integer> page,
+            @RequestParam(name = "offset", defaultValue = "10") @Min(1) @Max(10) final Optional<Integer> offset) {
+        final int size = offset.get() > 10 ? 10 : offset.get();
+        final int start = page.get() * size;
+
+        var skills = mapSkills();
+        final var totalElements = skills.size();
+
+        if (totalElements < start) {
+            return Collections.emptyList();
+        }
+
+        if (totalElements < start + size) {
+            return  skills.subList(start, totalElements);
+        }
+
+        return skills.subList(start, size);
     }
 
     @PostMapping
@@ -43,5 +61,9 @@ public class SkillsControllers {
     @PatchMapping
     public void update(Skill skill) {
         skills.replace(skill.getId(), skill.getName());
+    }
+
+    private List<Skill> mapSkills() {
+        return skills.entrySet().stream().map(item -> new Skill(item.getKey(), item.getValue())).toList();
     }
 }
